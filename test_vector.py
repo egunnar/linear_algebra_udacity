b0a458ad5c3cac57348ea4138cff23364ab149b0
Erik Gartz <email@gmail.com>
2016-09-25 14:59:17 -0400

    added tests

--- test_vector.py
+++ test_vector.py
@@ -1,12 +1,28 @@
 from vector import Vector
+import unittest
+import numpy as np
 
-a = Vector([8.218, -9.341])
-b = Vector([-1.129, 2.111])
-print(a + b)
+class TestVector(unittest.TestCase):
 
-a = Vector([7.119, 8.215])
-b = Vector([-8.223, 0.878])
-print(a - b)
+    def test_addition(self):
+        vector_1 = Vector([8.218, -9.341])
+        vector_2 = Vector([-1.129, 2.111])
+        expected_result = [7.089, -7.2299]
+        np.testing.assert_almost_equal(list(vector_1 + vector_2),
+            expected_result, 3)
 
-a = Vector([1.671, -1.012, -0.318])
-print(a.scalar_multipile(7.41))
+    def test_substraction(self):
+        vector_1 = Vector([7.119, 8.215])
+        vector_2 = Vector([-8.223, 0.878])
+        expected_result = [15.342, 7.337]
+        np.testing.assert_almost_equal(list(vector_1 - vector_2),
+            expected_result, 3)
+
+    def test_scalar_multiplication(self):
+        vector_1 = Vector([1.671, -1.012, -0.318])
+        expected_result = [12.38211, -7.49892, -2.35638]
+        np.testing.assert_almost_equal(list(vector_1.scalar_multipile(7.41)),
+            expected_result, 3)
+
+if __name__ == '__main__':
+    unittest.main()
